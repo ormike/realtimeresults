9a4d4eb17f980c077c073b79aa00f60d3f8dba60
Mike Bruns <ormike11@gmail.com>
2023-11-18 10:33:40 -0800

    Uploaded file gets to the upstream server

--- Container/realtimeresults/server.js
+++ Container/realtimeresults/server.js
@@ -9,6 +9,8 @@ const errorHandler = require('errorhandler');
 //const passport = require('passport');
 //const routes = require('./routes');
 const partials = require('express-partials');
+const multer = require('multer');
+const upload = multer({ dest: '.' });
 
 // Express configuration
 const app = express();
@@ -17,8 +19,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 app.use(partials());
 //app.use(cookieParser());
-app.use(express.json({ extended: false }));        // parse JSON bodies
-app.use(express.urlencoded({ extended: false }));  // parse URL-encoded bodies
+// app.use(express.json({ extended: false }));        // parse JSON bodies
+app.use(express.json()); //Used to parse JSON bodies
+// app.use(express.urlencoded({ extended: false }));  // parse URL-encoded bodies
+app.use(express.urlencoded());  // parse URL-encoded bodies
 app.use(errorHandler());
 //app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 //app.use(passport.initialize());
@@ -30,9 +34,15 @@ app.use(errorHandler());
 //app.use('/assets', express.static(__dirname + '/src/assets'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
+app.post('/upload', upload.none(), function(req, res) {
+    console.log(req.files);
+    console.log(req.body);
+    console.log(req.headers);
+    res.json({status: "ok"});
+});
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("server listening on port " + (process.env.PORT || 3000));
