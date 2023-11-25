856ef6c072cc9d613a7daeda0f25a5fe04f5db26
Mike Bruns <ormike11@gmail.com>
2023-11-24 19:56:31 -0800

    get nodejs from nodesource

--- Container/files/node.sh
+++ Container/files/node.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-HOME=/home/app exec chpst -u app /usr/local/bin/node /home/app/realtimeresults/server.js >> /tmp/node_run 2>&1
-
+HOME=/home/app exec chpst -u app /usr/bin/node /home/app/realtimeresults/server.js >> /tmp/node_run 2>&1
 
