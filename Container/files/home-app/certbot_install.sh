910363264cb9f253c0d60094353f923a2025f5c2
Mike Bruns <ormike11@gmail.com>
2023-11-27 06:34:16 -0800

    nginx use TLS

---
+++ Container/files/home-app/certbot_install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo apt install python3 python3-pip python3-venv
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot certbot-nginx
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+certbot --help
+
